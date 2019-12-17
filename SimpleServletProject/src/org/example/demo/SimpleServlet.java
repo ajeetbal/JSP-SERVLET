6742cfa382875cb639e4bc36caba29c917bb58aa
Ajeet Bal <ajeetbal7@gmail.com>
2019-12-17 18:10:54 +0530

    First Servlet demo

---
+++ SimpleServletProject/src/org/example/demo/SimpleServlet.java
@@ -0,0 +1,36 @@
+package org.example.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "A simple Servlet", urlPatterns = { "/SimpleServletPath" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SimpleServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+		response.getWriter().println("<h1>hello servlet</h1>");
+	}
+
+}
